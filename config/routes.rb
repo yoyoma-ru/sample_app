a4dd47a56e2cb3b30b67208518888ddc912ee149
vagrant <asjstksmr@gmail.com>
2020-05-22 10:17:20 +0000

    [finish]3-9

--- config/routes.rb
+++ config/routes.rb
@@ -12,4 +12,6 @@ Rails.application.routes.draw do
 
   patch 'todolists/:id' => 'todolists#update', as: 'update_todolist'
 
+  delete 'todolists/:id' => 'todolists#destroy', as: 'destroy_todolist'
+
 end
