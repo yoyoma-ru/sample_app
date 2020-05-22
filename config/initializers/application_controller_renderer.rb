16550e0b4c68899010131e0b15461fc1b1067d65
vagrant <asjstksmr@gmail.com>
2020-05-22 09:58:42 +0000

    [finish]3-8

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,5 @@
 #     https: false
 #   )
 # end
+
+Refile.secret_key = '5e2b7fc1eae0d4bebe3ca4f8f65e1e15ad8234ace9b0294d2a39404b5e097f8c5012cd6de6afa0293d1c4bb58805b72b4e4eb7883af981f7432f9c86531cf22d'
